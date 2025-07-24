7695c1729eb06c2be167e91201c6398067cbd729
estefany-123 <estefany_parada@soy.sena.edu.co>
2025-07-24 14:09:00 -0500

    Validacion de contraseñas

--- src/schemas/User.tsx
+++ src/schemas/User.tsx
@@ -61,9 +61,16 @@ export const UserSchema = z.object({
     password: z
         .string({ message: "Contraseña es obligatoria" })
         .min(8, { message: "Contraseña es requerida" }),
+    password_confirmation : z
+        .string()
+        .min(8, { message: "minimo 8 caracteres" }),
     fk_rol: z
         .number({ message: "Rol es requerido y debe ser un numero" })
 })
+    .refine((data)=> data.password === data.password_confirmation,{
+        message: "Las contraseñas no coinciden",
+        path:["password_confirmation"]
+    })
 
 export type User = z.infer<typeof UserSchema>
 
