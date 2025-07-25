f282c02b313b627a65aea367034f710dc085d9d1
Yurgen2007 <yurgenpv@gmail.com>
2025-07-24 19:47:11 -0500

    login

--- src/axios/Areas/getArea.ts
+++ src/axios/Areas/getArea.ts
@@ -2,6 +2,6 @@ import { Area } from "@/types/area";
 import { axiosAPI } from "../axiosAPI";
 
 export const getArea = async (): Promise<Area[]> => {
-  const res = await axiosAPI.get("areas");
+  const res = await axiosAPI.get("http://localhost:8000/api/areas");
   return res.data;
 };
--- src/axios/Usuarios/postLogin.ts
+++ src/axios/Usuarios/postLogin.ts
@@ -1,13 +1,8 @@
 import { axiosAPI } from "../axiosAPI";
-import {LoginCrede,LoginRes} from "@/types/Usuario"
+import { LoginCrede, LoginRes } from "@/types/Usuario";
 
-export async function postLogin(
-    data: LoginCrede
-  ): Promise<LoginRes> {
-    const response = await axiosAPI.post(
-      "/login",
-      data
-    );
-    console.log(response)
-    return response.data; 
-  }
+export async function postLogin(data: LoginCrede): Promise<LoginRes> {
+  const response = await axiosAPI.post("http://127.0.0.1:8000/ap/login", data);
+  console.log(response);
+  return response.data;
+}
