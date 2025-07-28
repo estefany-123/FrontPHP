2e37965053f52ec61621a6b0528cd1000cd81ac6
Yurgen2007 <yurgenpv@gmail.com>
2025-07-28 17:03:23 -0500

    commit

--- src/axios/Usuarios/postLogin.ts
+++ src/axios/Usuarios/postLogin.ts
@@ -2,7 +2,7 @@ import { axiosAPI } from "../axiosAPI";
 import { LoginCrede, LoginRes } from "@/types/Usuario";
 
 export async function postLogin(data: LoginCrede): Promise<LoginRes> {
-  const response = await axiosAPI.post("http://127.0.0.1:8000/ap/login", data);
+  const response = await axiosAPI.post("/login", data);
   console.log(response);
   return response.data;
 }
